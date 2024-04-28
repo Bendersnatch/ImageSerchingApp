e6b2df4eeb4718df8428abf92a4d16c96c0447fd
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2024-04-28 22:01:36 +0200

    Update ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import ImageShow from './ImageShow';
 
 function ImageList({images}) {
     const renderedImages = images.map((image) => {
-        return <ImageShow image={image} />;
+        return <ImageShow kay={image.id} image={image} />;
     });
     return <div>{renderedImages} </div>;
 }
